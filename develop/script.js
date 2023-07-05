ceebcac658939c3b86891305d69b7587571bd3d5
Andrew Stave <andrewdstave@gmail.com>
2023-07-05 13:41:59 -0700

    added all js

--- develop/script.js
+++ develop/script.js
@@ -6,29 +6,52 @@ function generatePassword() {
   var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
   var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numericChars = "0123456789";
-  var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
+  var specialChars = "!@$%^&*()_+[]{}|\~`:;<>,.?/=";
 
-  var passwordLength = prompt("How long would you like your password to be? (No more than 100 characters)");
+  var passwordLength = prompt("How many characters long would you like your password to be? (Between 9 and 128 characters)");
   passwordLength = parseInt(passwordLength);
 
-  while (isNaN(passwordLength) || passwordLength > 100) {
-    passwordLength = prompt("Invalid length. Password cannot be larger than 100 characters.");
+  while (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Invalid length. Password must be between 8 and 128 characters.");
     passwordLength = parseInt(passwordLength);
   }
-}
+
 
   var addLowercase = confirm("Would you like lowercase characters?");
   var addUppercase = confirm("Would you like uppercase characters?");
-  var adedNumbers = confirm("Would you like numeric characters?");
+  var addNumbers = confirm("Would you like numeric characters?");
   var addSpecial = confirm("Would you like special characters?");
 
-  while (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+  while (!addLowercase && !addUppercase && !addNumbers && !addSpecial) {
     alert("At least one character type must be selected!");
-    addLowercase = confirm("Would you like lowercase characters?");
-    addUppercase = confirm("Would you like uppercase characters?");
-    addNumbers = confirm("Would you like numeric characters?");
-    addSpecial = confirm("Would you like special characters?");
+    addLowercase = confirm("Would you like to include lowercase characters?");
+    addUppercase = confirm("Would you like to include characters?");
+    addNumbers = confirm("Would you like to include characters?");
+    addSpecial = confirm("Would you like to include characters?");
   }
+
+  var charset = "";
+  if (addLowercase) {
+    charset += lowercaseChars;
+  }
+  if (addUppercase) {
+    charset += uppercaseChars;
+  }
+  if (addNumbers) {
+    charset += numericChars;
+  }
+  if (addSpecial) {
+    charset += specialChars;
+  }
+
+  var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  return password;
+}
 
 // Write password to the #password input
 function writePassword() {
